116860849f217fd9bd318596639c7fea711fdd73
robburns <burrobert@gmail.com>
2016-06-03 23:13:08 +1000

    Initial Commit

---
+++ GaltonBox/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GaltonBox
+//
+//  Created by Robert Burns on 3/06/2016.
+//  Copyright © 2016 Robert Burns. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
